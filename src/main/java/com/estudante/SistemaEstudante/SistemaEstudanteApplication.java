d2f5043bfac858743cafde2d96b451a2d5a974f6
NicolasFSouza <nicolasouza200@gmail.com>
2026-02-26 20:54:20 -0300

    first commit

---
+++ src/main/java/com/estudante/SistemaEstudante/SistemaEstudanteApplication.java
@@ -0,0 +1,13 @@
+package com.estudante.SistemaEstudante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEstudanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEstudanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudante/SistemaEstudante/SistemaEstudanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudante.SistemaEstudante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEstudanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
