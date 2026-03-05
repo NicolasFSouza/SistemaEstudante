e87bfd2193258fc84208929b94df8578c7ce8667
NicolasFSouza <nicolasouza200@gmail.com>
2026-03-05 19:21:43 -0300

    commit de teste

---
+++ src/main/java/com/estudante/SistemaEstudante/controller/CourseController.java
@@ -0,0 +1,19 @@
+package com.estudante.SistemaEstudante.controller;
+import com.estudante.SistemaEstudante.model.CourseModel;
+import com.estudante.SistemaEstudante.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+
+@RestController
+@RequestMapping("/api/cursos")
+@RequiredArgsConstructor
+public class CourseController {
+
+
+}
